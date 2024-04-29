7b7535170c50d995466f19c4cbb5ef34665cfa37
Yasmine2702 <161338860+Yasmine2702@users.noreply.github.com>
2024-04-29 12:31:16 +0200

    Add files via upload

---
+++ my_stramlit_app_voiture.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns 
+
+
+st.title('Hello Wilders, bienvenue sur cette web app !')
+st.title('Analysons le dataset des voitures !')
+
+st.subheader("Importons d'abord le dataset:")
+
+link = "https://raw.githubusercontent.com/murpi/wilddata/master/quests/cars.csv"
+df_cars = pd.read_csv(link)
+st.write(df_cars)
+
+st.title('Graphique interactif de distribution')
+
+df_cars['continent'] = df_cars['continent'].apply(lambda x : x.replace('.', ''))
+option = st.selectbox(label= 'veuillez séléctionner un continent.', options= df_cars['continent'].unique())
+
+st.subheader("Nuage de points par continent")
+
+df_filtre = df_cars[df_cars["continent"]== option]
+
+viz_correlation= sns.scatterplot(x = df_filtre["hp"],
+                                 y = df_filtre["mpg"],
+                             hue = df_filtre["continent"])
+st.pyplot(viz_correlation.figure)
+                                
+   
+st.title('Graphique de corrélations filtré par région')
+
+st.subheader("Carte de chaleur par continent:")
+
+
+plt.figure(figsize=(10, 6))
+hm_cars = sns.heatmap(df_filtre.corr(numeric_only=True),annot=True, center=0, cmap=sns.color_palette("vlag", as_cmap=True) )
+st.pyplot(hm_cars.figure)
+
+
+
+
